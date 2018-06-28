928f7619679f63121104f2f9f929471c8baafb8f
Bryan Teng <bteng1226@gmail.com>
2018-06-28 07:57:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ new_hash = {}
 languages_by_style.each do |language_keys,languages_hash| 
   languages_hash.each do |name,type_hash| 
     type_hash.each do |type_key,type|
-      if new_hash
+      if new_hash[name].nil?
       new_hash[name] ={}
     new_hash[name][:style] ||= []
     new_hash[name][:style] << language_keys
